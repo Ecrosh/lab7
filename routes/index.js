98a0be416c78e6406c1f58cdb9738c89f50aadb7
Edward <eklau@ucsd.edu>
2016-02-18 21:54:01 +0000

    initial analytics lab

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,33 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	projects["grid"]  = false;
   	res.render('index', projects);
+  	var rand_num = Math.random();
+    console.log(rand_num);
+    
+    if (rand_num > 0.5) 
+    {
+        projects["grid"] = false;
+        res.render('index', projects);
+    } 
+    else
+    {
+        res.redirect('/grid');        
+    }
+};
+
+exports.viewGrid = function(req, res){
+	projects["grid"]  = true;
+  	res.render('index', projects);
+  	var rand_num = Math.random();
+  	if (rand_num > 0.5) 
+    {
+        projects["grid"] = true;
+        res.render('index', projects);
+    } 
+    else
+    {
+        res.redirect('/');
+    }
 };
